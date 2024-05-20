3c73b4391e09330514ff9cb4a4e8ac43cf991780
ShalkTiSofia <165826524+ShalkTiSofia@users.noreply.github.com>
2024-05-20 13:04:59 -0500

    casi final

--- js/script.js
+++ js/script.js
@@ -66,6 +66,34 @@ $(document).ready(function(){
       }
   ]
   });
+  $('.slider-nav2').slick({
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    dots: false,
+    centerMode: true,
+    focusOnSelect: true,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    responsive: [
+    {
+    breakpoint: 1024,
+    settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        infinite: true,
+        dots: false
+    }
+    },
+    {
+    breakpoint: 600,
+    settings: {
+        slidesToShow: 1,
+        slidesToScroll: 2
+    }
+    }
+]
+});
   $('.center').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
